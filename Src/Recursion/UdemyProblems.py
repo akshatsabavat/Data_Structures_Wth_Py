dc3402ad481898b948b8f42f600de16918b86b9c
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-01-22 23:34:42 +0530

    feat(Recursion --UdemyProblem(Problem 2: complete)

--- Src/Recursion/UdemyProblems.py
+++ Src/Recursion/UdemyProblems.py
@@ -1,4 +1,7 @@
 #Problem -- 1 (Power of Number)
+import re
+
+
 def powerOfNumber(number:int, power:int):
     if power == 0:
         return 1
@@ -8,4 +11,12 @@ def powerOfNumber(number:int, power:int):
         return number * powerOfNumber(number,power - 1);
 
 print(powerOfNumber(5,10))
+
+def factorial(n):
+    if n in [0,1]:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print(factorial(7))
 
