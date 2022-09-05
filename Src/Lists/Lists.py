5ae7e0293882cc79baaaa2ae560a733fd8a5ce03
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2022-09-05 20:05:59 +0530

    added list check and access methods

--- Src/Lists/Lists.py
+++ Src/Lists/Lists.py
@@ -3,6 +3,9 @@
 #can also have a collection of all types
 
 #int type
+from tabnanny import check
+
+
 integers = [10,11,9,10]
 
 #string type
@@ -12,3 +15,13 @@ strings = ['Eddy','Jhon','Karl']
 strings_and_integers = ["eddy",11,19,55.6,"Edmond"]
 
 print(strings_and_integers)
+
+#accessing list elemenst
+print(integers[1])
+print(integers[2])
+sum = integers[1] + integers[2]
+print(sum)
+
+#checking for values --> returns either true of false
+check = 11 in integers
+print(check)
